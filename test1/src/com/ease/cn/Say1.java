64e0e0e2cbb3812101d967034eabba72a0acc54d
zhaox <1582204460@qq.com>
2022-11-26 22:00:20 +0800

    将java工程添加到版本库

---
+++ test1/src/com/ease/cn/Say1.java
@@ -0,0 +1,9 @@
+package com.ease.cn;
+
+public class Say1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello ease!");
+	}
+}
---
+++ test1/src/com/ease/cn/Say2.java
@@ -0,0 +1,9 @@
+package com.ease.cn;
+
+public class Say2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("256699");
+	}
+}
